674fe16168e5d29c337c317731b646c4fbbfb6e2
Shatrov-Mihail <78844887@mail.ru>
2024-08-23 20:47:36 +0500

    1233

--- my-app/src/components/hooks/index.js
+++ my-app/src/components/hooks/index.js
@@ -1,10 +1,4 @@
-<<<<<<< Updated upstream
-export * from './add-new-case'
-export * from './update-case'
-export * from './delete-case'
-=======
 export * from "./use-add-new-case";
 export * from "./use-update-case";
 export * from "./use-delete-case";
->>>>>>> Stashed changes
 
